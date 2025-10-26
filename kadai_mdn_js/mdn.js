da7f0e2b332ca4404bc54f7ca7f5afe7d01e3f12
oohamahama3 <naomihama3@gmail.com>
2025-10-26 20:32:47 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,4 +1,4 @@
 const date = new Date();
+let mm = date.getMonth() + 1
 
-console.log(date.getFullYear() + '年' + date.getMonth()+1 + '月' + date.getDate()+'日');
-
+console.log(date.getFullYear() + '年' + mm + '月' + date.getDate()+'日');
