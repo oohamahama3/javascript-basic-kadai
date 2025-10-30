9ce06aed3fca73da087fc089a51a213c30043f1e
oohamahama3 <naomihama3@gmail.com>
2025-10-30 20:42:30 +0900

    課題提出

---
+++ kadai_020/event.js
@@ -0,0 +1,12 @@
+//======== 前略 ========
+
+// 定数に代入する
+const myBtn = document.getElementById('btn');
+// 定数に代入する
+const myTxt = document.getElementById('text');
+// 
+myBtn.addEventListener('click', () => {
+ 
+   myTxt.textContent = 'ボタンをクリックしました';
+ });
+
